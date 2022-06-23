878bb708f7f66d845bcc1d906648813fe78bb226
eduar <eduar@DESKTOP-IM1CNCF>
2022-06-23 01:23:56 -0600

    Clase 5 Reflejar informacion, reutilizar plantillas

--- src/main/java/com/tienda/controller/indexController.java
+++ src/main/java/com/tienda/controller/indexController.java
@@ -3,6 +3,7 @@ package com.tienda.controller;
 
 import com.tienda.dao.Clientedao;
 import com.tienda.domain.Cliente;
+import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class indexController {
     
     @Autowired
-    private Clientedao clienteDao;
+    private ClienteService clienteService;
     
     @GetMapping("/")
     public String inicio(Model model){
@@ -31,8 +32,32 @@ public class indexController {
         //var clientes = Arrays.asList(cliente, cliente2);
         var clientes = Arrays.asList();*/
         
-        var clientes = clienteDao.findAll();
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+    
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente){
+        return "modificarCliente";
+    }
+    
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect/";
+    }
+    
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model){
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
