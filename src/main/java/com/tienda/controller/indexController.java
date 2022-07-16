d2f93a86d1e64f066c905ad0da4a4d8326cf3b00
eduar <eduar@DESKTOP-IM1CNCF>
2022-07-16 01:09:25 -0600

    Semana 8: Controllers y bases de datos

--- src/main/java/com/tienda/controller/indexController.java
+++ src/main/java/com/tienda/controller/indexController.java
@@ -3,6 +3,7 @@ package com.tienda.controller;
 
 import com.tienda.dao.Clientedao;
 import com.tienda.domain.Cliente;
+import com.tienda.service.ArticuloService;
 import com.tienda.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 public class indexController {
     
     @Autowired
-    private ClienteService clienteService;
+    private ArticuloService articuloService;
     
     @GetMapping("/")
     public String inicio(Model model){
@@ -32,32 +33,10 @@ public class indexController {
         //var clientes = Arrays.asList(cliente, cliente2);
         var clientes = Arrays.asList();*/
         
-        var clientes = clienteService.getClientes();
-        model.addAttribute("clientes", clientes);
+        var articulos = articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
+        
         return "index";
     }
     
-    @GetMapping("/nuevoCliente")
-    public String nuevoCliente(Cliente cliente){
-        return "modificarCliente";
-    }
-    
-    @PostMapping("/guardarCliente")
-    public String guardarCliente(Cliente cliente) {
-        clienteService.save(cliente);
-        return "redirect/";
-    }
-    
-    @GetMapping("/modificarCliente/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
-        cliente = clienteService.getCliente(cliente);
-        model.addAttribute("cliente", cliente);
-        return "modificarCliente";
-    }
-    
-    @GetMapping("/eliminarCliente/{idCliente}")
-    public String eliminarCliente(Cliente cliente){
-        clienteService.delete(cliente);
-        return "redirect:/";
-    }
 }
