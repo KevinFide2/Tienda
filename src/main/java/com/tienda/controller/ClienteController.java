a9050d9f1bb3ffc81e97506d90d88c81320aac25
eduar <eduar@DESKTOP-IM1CNCF>
2022-08-06 14:21:28 -0600

    Bootstrap

--- src/main/java/com/tienda/controller/ClienteController.java
+++ src/main/java/com/tienda/controller/ClienteController.java
@@ -16,13 +16,20 @@ public class ClienteController {
     private ClienteService clienteService;
     
     @GetMapping("/cliente/listado")
-    public String inicio(Model model){
+    public String inicio(Model model) {
+        var clientes=clienteService.getClientes();
         
-        var clientes = clienteService.getClientes();
-        model.addAttribute("clientes", clientes);
+        var limiteTotal=0;
+        for (var c: clientes) {
+            limiteTotal+=c.getCredito().getLimite();
+        }
+        model.addAttribute("limiteTotal",limiteTotal);
+        model.addAttribute("totalClientes",clientes.size());
         
+        model.addAttribute("clientes",clientes);
         return "/cliente/listado";
     }
+
     
     @GetMapping("cliente/nuevo")
     public String nuevoCliente(Cliente cliente){
