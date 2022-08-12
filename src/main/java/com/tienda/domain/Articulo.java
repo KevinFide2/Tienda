6ffcf6737ecbb810058e8e1d4bd536312667a9a6
eduar <eduar@DESKTOP-IM1CNCF>
2022-08-12 01:30:59 -0600

    Clase 13 Mostrar Carrito desde base de datos y actualizar

--- src/main/java/com/tienda/domain/Articulo.java
+++ src/main/java/com/tienda/domain/Articulo.java
@@ -1,5 +1,5 @@
-
 package com.tienda.domain;
+
 import java.io.Serializable;
 import javax.persistence.*;
 import lombok.Data;
@@ -7,29 +7,30 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "articulo")
-public class Articulo implements Serializable{
-    
+public class Articulo implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idArticulo;
+    @Column(name = "id_articulo")
+    private Long idArticulo; // Hibernate lo transforma en id_categoria    
     private Long idCategoria;
     private String descripcion;
     private String detalle;
+    private double precio;
     private int existencias;
     private boolean activo;
 
     public Articulo() {
     }
 
-    public Articulo(Long idCategoria, String descripcion, String detalle, int existencias, boolean activo) {
+    public Articulo(Long idCategoria, String descripcion, String detalle, double precio, int existencias, boolean activo) {
         this.idCategoria = idCategoria;
         this.descripcion = descripcion;
         this.detalle = detalle;
+        this.precio = precio;
         this.existencias = existencias;
         this.activo = activo;
     }
-
-   
 }
+
